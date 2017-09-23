588484c3aaeb56220c2fc064810a250a8bd68f14
Brandon  Harris <cheeridon@gmail.com>
2017-09-23 17:12:48 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,12 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(number)
+  idx = 0
+  while idx < number
+    sleep(number)
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
